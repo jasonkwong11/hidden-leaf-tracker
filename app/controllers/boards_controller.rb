7698f738afcd8969ae2bef729c63feb470cdcb2d
Jason Kwong <jasonkwong11@gmail.com>
2017-07-09 12:35:17 -0700

    allow duplicates

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -118,12 +118,12 @@ class BoardsController < ApplicationController
 
       if reservation_hash[:room].include?("Tacoma")
         reservation_hash[:room] = reservation_hash[:room][-2..-1]
-        tacoma_board.reservations.find_or_create_by(reservation_hash) if reservation_hash[:checkout] >= Date.today && !Reservation.find_by(customer: reservation_hash[:customer])
+        tacoma_board.reservations.find_or_create_by(reservation_hash) if reservation_hash[:checkout] >= Date.today
       end
 
       if reservation_hash[:room].include?("Airport")
         reservation_hash[:room] = reservation_hash[:room][-1]
-        seatac_board.reservations.find_or_create_by(reservation_hash) if reservation_hash[:checkout] >= Date.today && !Reservation.find_by(customer: reservation_hash[:customer])
+        seatac_board.reservations.find_or_create_by(reservation_hash) if reservation_hash[:checkout] >= Date.today
       end
     end
     redirect_to '/'
